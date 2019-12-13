db2718226538fe444af4c1c86cb398ce2585395a
Jirawat I <nodtem66@gmail.com>
2019-12-14 02:57:04 +0700

    fast commit

---
+++ Scaffolder_2.cpp
@@ -0,0 +1,98 @@
+﻿#include <igl/writePLY.h>
+#include <igl/readSTL.h>
+#include <igl/copyleft/marching_cubes.h>
+#include <igl/fast_winding_number.h>
+#include <Eigen/Core>
+#include "Scaffolder_2.h"
+
+int main(int argc, char* argv[])
+{
+    // Define default parameters
+    bool verbose = true;
+    bool use_smooth_shape = false;
+    bool use_smooth_mesh = false;
+    bool is_test = false;
+    int precision = 6;
+    uint16_t border_offset = 2;
+    double thickness = 0.5;
+    size_t grid_size = 100;
+    double coff = pi;
+    // file parameters
+    std::string filename = "out";
+    std::string format = "ply";
+
+    // Inline mesh of a cube
+    Eigen::MatrixXd V1,V2;
+    Eigen::MatrixXi F1,F2;
+    {
+        Eigen::MatrixXd N;
+        igl::readSTL("2.stl", V1, F1, N);
+    }
+    
+    const Eigen::RowVector3d V1min = V1.colwise().minCoeff();
+    const Eigen::RowVector3d V1max = V1.colwise().maxCoeff();
+    const Eigen::RowVector3d L = V1max - V1min;
+    const Eigen::RowVector3d delta = L / grid_size;
+    Iso_cuboid_condition condition(V1min(0), V1min(1), V1min(2), L(0), L(1), L(2));
+    Implicit_function fn(condition, schwarzp, coff, thickness);
+    const Eigen::RowVector3d V1min1 = V1min - border_offset * delta;
+    grid_size += 2*(size_t)(border_offset);
+
+    if (verbose) std::cout << "Bounding Box: " << V1min << ' ' << V1max << std::endl;
+    if (verbose) std::cout << "Length: " << L << std::endl;
+    //if (verbose) std::cout << "delta: " << delta << std::endl;
+    if (verbose) std::cout << "[Generating grid] " << grid_size * grid_size * grid_size << ' (' << grid_size << '*' << grid_size << '*' << grid_size << ') ';
+    Eigen::MatrixXd GV(grid_size * grid_size * grid_size, 3);
+    Eigen::VectorXd Fxyz(grid_size * grid_size * grid_size);
+    for (size_t k = 0; k < grid_size; k++) {
+        const double z = V1min1(2) + k * delta(2);
+        for (size_t j = 0; j < grid_size; j++) {
+            const double y = V1min1(1) + j * delta(1);
+            for (size_t i = 0; i < grid_size; i++) {
+                const double x = V1min1(0) + i * delta(0);
+                const size_t index = i + grid_size * (j + grid_size * k);
+                GV.row(index) = Eigen::RowVector3d(x, y, z);
+            }
+        }
+    }
+    if (verbose) std::cout << "OK" << std::endl;
+
+    if (verbose) std::cout << "[Calculating Wind number] ";
+    Eigen::VectorXd W;
+    {
+        igl::FastWindingNumberBVH bvh;
+        igl::fast_winding_number(V1, F1, 2, bvh);
+        igl::fast_winding_number(bvh, 2, GV, W);
+    }
+    if (verbose) std::cout << "OK" << std::endl;
+    if (verbose) std::cout << "-- Info: W>=0.5 " << (W.array() >= 0.5).count() << std::endl;
+
+    if (verbose) std::cout << "[Generating isosurface Fxyz] ";
+    for (size_t k = 0; k < grid_size; k++) {
+        const double z = V1min1(2) + k * delta(2);
+        for (size_t j = 0; j < grid_size; j++) {
+            const double y = V1min1(1) + j * delta(1);
+            for (size_t i = 0; i < grid_size; i++) {
+                const double x = V1min1(0) + i * delta(0);
+                const size_t index = i + grid_size * (j + grid_size * k);
+                Fxyz(index) = W(index) >= 0.5 ? fn(x, y, z) : 1.0;
+            }
+        }
+    }
+    if (verbose) std::cout << "OK" << std::endl;
+    
+    if (verbose) std::cout << "[Marching Cube] ";
+    igl::copyleft::marching_cubes(Fxyz, GV, grid_size, grid_size, grid_size, V2, F2);
+    if (verbose) std::cout << "OK" << std::endl;
+
+    
+    if (verbose) std::cout << "[Writing PLY] ";
+    igl::writePLY("out.ply", V2, F2);
+    if (verbose) std::cout << "OK" << std::endl;
+    /*
+    if (verbose) std::cout << "[Writing OBJ] ";
+    igl::writeOBJ("out.obj", SV, SF);
+    if (verbose) std::cout << "OK" << std::endl;
+    */
+    return 0;
+}
---
+++ Scaffolder_2.h
@@ -0,0 +1,101 @@
+﻿// Scaffolder_2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#ifndef SCAFFOLDER_2_H
+#define SCAFFOLDER_2_H
+
+#include <iostream>
+#include <cmath>
+#include <string.h>
+#include <algorithm>
+
+typedef double FT;
+const FT pi = 3.141592653589793238462643383279502884L;
+const FT esp = 1e-6;
+typedef FT(Function_3)(FT, FT, FT);
+
+inline FT schwarzp(FT x, FT y, FT z) {
+	return cos(x) + cos(y) + cos(z);
+}
+inline FT sphere(FT x, FT y, FT z, FT r) {
+	return x * x + y * y + z * z - r * r;
+}
+inline FT DFx(Function_3 F, FT x, FT y, FT z) {
+	return (F(x + esp, y, z) - F(x, y, z)) / esp;
+}
+inline FT DFy(Function_3 F, FT x, FT y, FT z) {
+	return (F(x, y + esp, z) - F(x, y, z)) / esp;
+}
+inline FT DFz(Function_3 F, FT x, FT y, FT z) {
+	return (F(x, y, z + esp) - F(x, y, z)) / esp;
+}
+inline FT DF(FT dx, FT dy, FT dz) {
+	return sqrt(dx * dx + dy * dy + dz * dz);
+}
+inline FT IsoThicken(Function_3 F, FT x, FT y, FT z, FT thickness = 1.0) {
+	FT dx = DFx(schwarzp, x, y, z), dy = DFy(schwarzp, x, y, z), dz = DFz(schwarzp, x, y, z);
+	FT const df = DF(dx, dy, dz);
+	dx *= 0.5 * thickness / df;
+	dy *= 0.5 * thickness / df;
+	dz *= 0.5 * thickness / df;
+	FT const iso1 = schwarzp(x + dx, y + dy, z + dz);
+	FT const iso2 = schwarzp(x - dx, y - dy, z - dz);
+	return iso1 * iso2;
+}
+inline FT max_3(FT a, FT b, FT c) {
+	return (std::max)((std::max)(a, b), c);
+}
+
+class Function {
+public:
+	virtual FT operator()(FT, FT, FT) = 0;
+};
+/* Function reture the isocuboid boundary
+ * where minimum point origins at (origin_x, origin_y, origin_z) and 
+ * maximum point is (origin_x+w, origin_y+h, origin_z+t)
+ * return 0 if the point on the surface
+ * return -1 if the point inside or 1 if outside
+ */
+class Iso_cuboid_condition : public Function {
+private:
+	const FT origin_x;
+	const FT origin_y;
+	const FT origin_z;
+	const FT w;
+	const FT h;
+	const FT t;
+public:
+	Iso_cuboid_condition(FT x = 0, FT y = 0, FT z = 0, FT w = 1, FT h = 1, FT t = 1) :
+		origin_x(x), origin_y(y), origin_z(z), w(w), h(h), t(t) {}
+	FT operator()(FT x, FT y, FT z) {
+		const FT dx = 0.5 * w, dy = 0.5 * h, dz = 0.5 * t;
+		if (max_3(
+			abs(x - origin_x - dx) / dx,
+			abs(y - origin_y - dy) / dy,
+			abs(z - origin_z - dz) / dz
+		) > 1)
+			return 1.001;
+		return -.001;
+	}
+};
+/* Function return schwarzp isosurface function with following parameters:
+ * double thickness: adding the boundary by +-(thickness/2) to the isosurface function
+ */
+class Implicit_function : public Function {
+private:
+	const Function_3& isosurface;
+	const double coff;
+	const double thickness;
+	Function& condition;
+public:
+	Implicit_function(Function& condition, const Function_3& isosurface, const double coff, const double thickness) :
+		condition(condition), isosurface(isosurface), coff(coff), thickness(thickness) {}
+	FT operator()(FT x, FT y, FT z) {
+		if (condition(x, y, z) <= 0) {
+			return IsoThicken(isosurface, x * coff, y * coff, z * coff, thickness);
+		}
+		return 1.0;
+	}
+};
+#endif
