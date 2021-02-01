74a328b67066d7d43f51bca95c9e7552aaf7dd0e
Jirawat I <nodtem66@gmail.com>
2021-02-01 19:20:34 +0700

    add external download to cmake

---
+++ scripts/tar_external.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+echo -n "Create external.tar.gz    "
+if [ ! -e external.tar.gz ]; then
+  tar --directory=../ \
+      --exclude=external/sol2/docs \
+      --exclude=external/examples/docs \
+      --exclude=external/tests/scripts \
+      --exclude=external/tests/docs \
+      --exclude=external/tbb/doc \
+      --exclude=external/tbb/examples \
+      --exclude=external/tbb/test \
+      --exclude=external/vcglib/docs \
+      --exclude=external/vcglib/apps \
+      --exclude=external/libigl/tutorial \
+      --exclude=external/libigl/tests \
+      --exclude=external/libigl/external \
+      --exclude-vcs --exclude-vcs-ignores \
+      -czf external.tar.gz external && echo "[OK]"
+else
+  echo "[Exists]"
+fi
